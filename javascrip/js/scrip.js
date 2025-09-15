99cbf51b51166c4e17c88bfc89b986a00c36d6e2
fran-sava <mosafran95@gmail.com>
2025-09-15 00:44:41 -0300

    Creacion de carpeta index.html en javascript y scrip.js

---
+++ javascrip/js/scrip.js
@@ -0,0 +1,6 @@
+/*Introduccion*/
+//Comentario de linea
+alert("Hola mundo desde js con alert");
+document.write("<h1>Hola mundo desde js con document write</h1><he>");
+console.log("Hola esto es un mensaje en consola desde el script");
+
