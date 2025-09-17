ae454024b6f3a1bdd4d3eff5d1586f4964ea9845
fran-sava <mosafran95@gmail.com>
2025-09-17 01:51:00 -0300

    Ejercicios realizados 1-intro, 2-variables, 3-tipos de datos, 4-condicionales

---
+++ javascrip/3-tipos-de-datos/js/script.js
@@ -0,0 +1,90 @@
+/*Tipos de datos*/
+//Numericos
+
+var nro1 = 50;
+var nro2 = 25;
+//Operadores
+var suma = nro1+nro2;
+var resta = nro1-nro2;
+var multi = nro1*nro2;
+var divi = nro1/nro2;
+var porcentaje = nro1%nro2;
+var decimal = 25.5;
+var verdadero = true;
+var falso = false;
+
+var texto = "El resultado de la operacion es: ";
+document.write(texto, suma, "<br>");
+document.write(texto, resta, "<br>");
+document.write(texto, multi, "<br>");
+document.write(texto, divi, "<br>");
+document.write(texto, porcentaje, "<br>");
+document.write(texto, decimal, "<br>");
+document.write(texto, verdadero,"<br>", falso,"<br>");
+
+//funciones para convertir datos
+document.write("<hr>");
+document.write("<h1>Funciones para convertir datos</h1>");
+
+var numerico = '30';
+document.write(numerico);
+var tipodatostring = typeof(numerico);
+document.write(tipodatostring,"<br>");
+//convertir string a numero
+
+
+
+var numerico_convertido = Number(numerico);
+document.write(numerico_convertido);
+typeof(numerico_convertido);
+
+//ver tipo de dato
+var tipodato = typeof(numerico_convertido);
+document.write(tipodato,"<br>");
+
+//convertir nro a string
+
+var nroentero=40;
+var convertidostring=String(nroentero,"<br>");
+var tipostring = typeof(convertidostring);
+document.write(convertidostring,tipostring);
+
+//parsetInt
+var nrostring = '100';
+var parse = parseInt(nrostring);
+document.write(parse);
+console.log(typeof(parse));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
