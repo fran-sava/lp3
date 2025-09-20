d0c8f47a2cbfb6922eef6439aeb233046bd4423d
fran-sava <mosafran95@gmail.com>
2025-09-20 18:33:09 -0300

    Ejercicios realizados javascript-10-ejercicios-1.js-2.html

---
+++ javascrip/10-ejercicios/js/2.js
@@ -0,0 +1,22 @@
+/*Utilizar un bucle y mostrar las medidas y el promedio de un usuario
+hasta que el usuario ingrese un nro negativo
+*/
+
+var suma = 0;
+contador = 0;
+do{
+    var numero = parseInt(prompt('Ingrese nros hasta que ingreses un negativo', 0));
+    if(contador ==3){
+        alert('Si queres parar ingresa un valor negativo');
+    }
+    if (isNaN(numero)){
+        numero=0;
+    } else if (numero>=0){
+        suma=suma + numero;
+        contador++
+    }
+
+}while(numero>=0){
+    alert('La suma es: '+suma);
+    alert('El promedio es: '+suma / contador);
+}
