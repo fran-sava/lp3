407097ad7838dc6c0e98970e4e5f5850b4d0a8fd
fran-sava <mosafran95@gmail.com>
2025-10-21 07:57:02 -0300

    Ejercicios realizados 3,4 .PHP

---
+++ php/4.php
@@ -0,0 +1,54 @@
+<h1>Estructura for</h1>
+<?php
+/*
+for(variable iteradora; condicion variable; variable++){
+    bloque de instrucciones
+}
+*/
+for($i = 0; $i <=10; $i++){
+    echo "El nro es: $i <br>";
+}
+
+echo "<h1>Foreach para recorrer matrices </h1>";
+$colores = array("rojo", "verde", "azul", "amarillo");
+foreach($colores as $valores){
+    echo "$valores <br>";
+}
+
+echo "<h1>Foreach para recorrer matrices con indice de datos</h1>";
+$año = array("Pedro"=>"35", "Juan" =>"37", "Maria" =>"28");
+foreach($año as $nombre => $edad){
+    echo "$nombre = $edad <br>";
+}
+
+echo "<h1>Break para parar la ejecucion</h1>";
+    for ($a = 0; $a <10; $a++){
+        if($a ==5){
+        break;
+        }
+        echo "El nro es: $a <br>";
+    }
+
+echo "<h1>Continue para parar la ejecucion</h1>";
+    for ($a = 0; $a <10; $a++){
+        if($a ==5){
+        continue;
+        }
+        echo "El nro es: $a <br>";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
